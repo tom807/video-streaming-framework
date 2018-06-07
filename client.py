7ecacf844f2059e357fa0929264142e1ba1d1146
chenwei <c.weiii94@gmail.com>
2018-06-08 03:33:34 +0800

    Use multi-thread to do different tasks (Close #1)

--- client.py
+++ client.py
@@ -7,71 +7,103 @@ import transfer
 cap = cv2.VideoCapture("/dev/video0")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address_server = ('127.0.0.1', 9997)
+address_server = ('127.0.0.1', 9999)
 sock.connect(address_server)
 
 framebuffer = []
 displaybuffer = []
 transferbuffer = []
+receiverbuffer = []
 
 FRAME_BUFFER_SIZE = 125
 DISPLAY_BUFFER_SIZE = 10
 TRANSFER_BUFFER_SIZE = 20
+RECEIVER_BUFFER_SIZE = 20
 
-class Display(threading.Thread):
-    def __init__(self, lock, threadName):
-        super(Display, self).__init__(name = threadName)
-        self.lock = lock
+class fetchFrame(threading.Thread):
+    def __init__(self, display_lock, transfer_lock, threadName):
+        super(fetchFrame, self).__init__(name = threadName)
+        self.dlock = display_lock
+        self.tlock = transfer_lock
 
     def run(self):
-        while 2 > 1:
-            if len(displaybuffer) > BUFFER_SIZE:
-                self.lock.acquire()
-                frame = displaybuffer.pop(0)
-                self.lock.release()
-                #cv2.imshow('display', frame)
+        while True:
+            ret, frame = cap.read()
+            framebuffer.append(frame)
 
-'''
-lock = threading.Lock()
-Display(lock, "display").start()
-'''
+            if len(framebuffer) > FRAME_BUFFER_SIZE:
+                self.dlock.acquire()
+                displaybuffer.append(framebuffer[0])
+                self.dlock.release()
+
+                self.tlock.acquire()
+                transferbuffer.append(framebuffer[0])
+                self.tlock.release()
+
+                framebuffer.pop(0)
+
+class sendFrame(threading.Thread):
+    def __init__(self, transfer_lock, threadName):
+        super(sendFrame, self).__init__(name = threadName)
+        self.tlock = transfer_lock
+
+    def run(self):
+        while True:
+            if len(transferbuffer) > TRANSFER_BUFFER_SIZE:
+                self.tlock.acquire()
+                string_transfer_data = transfer.convert2jpg(transferbuffer.pop(0))
+                self.tlock.release()
+
+                print("Send size to server")
+                transfer.trans_size(sock, len(string_transfer_data))
+                print("Sent: " + str(len(string_transfer_data)))
+
+                print("Send image to server")
+                transfer.trans_frame(sock, string_transfer_data)
+                print("Image Sent!")
+
+class recvFrame(threading.Thread):
+    def __init__(self, receive_lock, threadName):
+        super(recvFrame, self).__init__(name = threadName)
+        self.rlock = receive_lock
+
+    def run(self):
+        while True:
+            print("Waiting Server size")
+            receive_length = transfer.recv_size(sock, 16)
+            length = int(receive_length.decode())
+            print("Receive: " + str(length))
+
+            print("Waiting Server image")
+            stringData = transfer.recv_frame(sock, length)
+            print("Image received successfully!")
+
+            decimg = transfer.convert2frame(stringData)
+
+            self.rlock.acquire()
+            receiverbuffer.append(decimg)
+            self.rlock.release()
+
+display_lock = threading.Lock()
+transfer_lock = threading.Lock()
+receive_lock = threading.Lock()
+
+fetchFrame(display_lock, transfer_lock, "test").start()
+sendFrame(transfer_lock, "test").start()
+recvFrame(receive_lock, "test").start()
 
 while True:
-    ret, frame = cap.read()
-    framebuffer.append(frame)
-
-    if len(framebuffer) > FRAME_BUFFER_SIZE:
-        cv2.imshow('process', framebuffer[0])
-        displaybuffer.append(framebuffer[0])
-        transferbuffer.append(framebuffer[0])
-        framebuffer.pop(0)
-
     if len(displaybuffer) > DISPLAY_BUFFER_SIZE:
-        cv2.imshow('display', displaybuffer.pop(0))
+        display_lock.acquire()
+        disimg = displaybuffer.pop(0)
+        #cv2.imshow('display', displaybuffer.pop(0))
+        display_lock.release()
+        cv2.imshow('display', disimg)
 
-    if len(transferbuffer) > TRANSFER_BUFFER_SIZE:
-        string_transfer_data = transfer.convert2jpg(transferbuffer.pop(0))
-        
-        print("Send size to server")
-        transfer.trans_size(sock, len(string_transfer_data))
-        print("Sent: " + str(len(string_transfer_data)))
-
-        print("Send image to server")
-        transfer.trans_frame(sock, string_transfer_data)
-        #sock.send(string_transfer_data)
-        print("Image Sent!")
-
-        # Get video back
-        print("Waiting Server size")
-        receive_length = transfer.recv_size(sock, 16)
-        length = int(receive_length.decode())
-        print("Receive: " + str(length))
-
-        print("Waiting Server image")
-        stringData = transfer.recv_frame(sock, length)
-        print("Image received successfully!")
-
-        decimg = transfer.convert2frame(stringData)
+    if len(receiverbuffer) > RECEIVER_BUFFER_SIZE:
+        receive_lock.acquire()
+        decimg = receiverbuffer.pop(0)
+        receive_lock.release()
         cv2.imshow("client", decimg)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- server.py
+++ server.py
@@ -2,40 +2,79 @@ import socket
 import cv2
 import numpy
 import time
+import threading
 import transfer
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = ('127.0.0.1', 9997)
+address = ('127.0.0.1', 9999)
 s.bind(address) 
 s.listen(True) 
 print ('Waiting for images...')
 conn, addr = s.accept()
 
-while True:
-    print("Waiting Client size")
-    receive_length = transfer.recv_size(conn, 16)
-    length = int(receive_length.decode())
-    print("Receive: " + str(length))
+receiverbuffer = []
+transferbuffer = []
 
-    print("Waiting Client image")
-    stringData = transfer.recv_frame(conn, length)
-    print('Image recieved successfully!')
+class recvFrame(threading.Thread):
+    def __init__(self, receive_lock, threadName):
+        super(recvFrame, self).__init__(name = threadName)
+        self.rlock = receive_lock
 
-    # Decode and display
-    decimg = transfer.convert2frame(stringData)
-    cv2.imshow('SERVER', decimg)
+    def run(self):
+        while True:
+            print("Waiting client size")
+            receive_length = transfer.recv_size(conn, 16)
+            length = int(receive_length.decode())
+            print("Receive: " + str(length))
 
-    # Send Back
-    string_transfer_data = transfer.convert2jpg(decimg)
+            print("Waiting client image")
+            stringData = transfer.recv_frame(conn, length)
+            print("Image received successfully!")
+
+            decimg = transfer.convert2frame(stringData)
+
+            self.rlock.acquire()
+            receiverbuffer.append(decimg)
+            self.rlock.release()
+
+class sendFrame(threading.Thread):
+    def __init__(self, transfer_lock, threadName):
+        super(sendFrame, self).__init__(name = threadName)
+        self.tlock = transfer_lock
+
+    def run(self):
+        while True:
+            if len(transferbuffer) > 0:
+                self.tlock.acquire()
+                string_transfer_data = transfer.convert2jpg(transferbuffer.pop(0))
+                self.tlock.release()
  
-    print("Send size to Client")
-    transfer.trans_size(conn, len(string_transfer_data))
-    print("Sent: " + str(len(string_transfer_data)))
+                print("Send size to client")
+                transfer.trans_size(conn, len(string_transfer_data))
+                print("Sent: " + str(len(string_transfer_data)))
 
-    print("Send image to Client")
-    transfer.trans_frame(conn, string_transfer_data)
-    #conn.send(string_transfer_data)
-    print("Image Sent!")
+                print("Send image to client")
+                transfer.trans_frame(conn, string_transfer_data)
+                print("Image Sent!")
+
+
+receive_lock = threading.Lock()
+transfer_lock = threading.Lock()
+
+sendFrame(transfer_lock, "test").start()
+recvFrame(receive_lock, "test").start()
+
+while True:
+    if len(receiverbuffer) > 0:
+        receive_lock.acquire()
+        decimg = receiverbuffer.pop(0)
+        receive_lock.release()
+        cv2.imshow('SERVER', decimg)
+
+        # Send Back
+        transfer_lock.acquire()
+        transferbuffer.append(decimg)
+        transfer_lock.release()
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
